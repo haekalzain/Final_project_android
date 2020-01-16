0eeac2e5f3b656df5f83df67e7c64ad31e7b89fb
haekalzain <haekalzain77@gmail.com>
2020-01-16 22:20:45 +0700

    ubah intent dari splash ke login ke main

--- Final_Project/app/src/main/java/com/example/final_project/controller/LoginActivity.java
+++ Final_Project/app/src/main/java/com/example/final_project/controller/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.final_project.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.final_project.R;
 
@@ -12,5 +14,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },2000);
     }
 }
--- Final_Project/app/src/main/java/com/example/final_project/controller/MainActivity.java
+++ Final_Project/app/src/main/java/com/example/final_project/controller/MainActivity.java
@@ -14,12 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent= new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
-            }
-        },2000);
+        
     }
 }
