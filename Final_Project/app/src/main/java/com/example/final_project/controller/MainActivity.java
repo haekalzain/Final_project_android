2d1eb10d41fed4b725266eba0877e58aaf0400c5
haekalzain <haekalzain77@gmail.com>
2020-01-17 16:04:15 +0700

    push project

--- Final_Project/app/src/main/java/com/example/final_project/controller/MainActivity.java
+++ Final_Project/app/src/main/java/com/example/final_project/controller/MainActivity.java
@@ -14,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this,NasabahActivity.class);
+                startActivity(intent);
+            }
+        },2000);
     }
 }
--- Final_Project/app/src/main/java/com/example/final_project/controller/NasabahActivity.java
+++ Final_Project/app/src/main/java/com/example/final_project/controller/NasabahActivity.java
@@ -1,14 +1,67 @@
 package com.example.final_project.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.final_project.R;
+import com.example.final_project.fragment.DaftarNasabahFragment;
+import com.example.final_project.fragment.ListNasabahFragment;
 
 public class NasabahActivity extends AppCompatActivity {
+    FrameLayout framenasabah;
+    FragmentTransaction fm;
+    LinearLayout lldatanasabah,lldaftarnasabah;
+    TextView datanasabah,daftarnasabah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nasabah);
+        findViewById();
+        initAction();
+        lldatanasabah.callOnClick();
+
+    }
+
+     void initAction() {
+    lldatanasabah.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            fm = getSupportFragmentManager().beginTransaction();
+            fm.replace(R.id.framenasabah,new ListNasabahFragment());
+            fm.commit();
+            lldatanasabah.setBackgroundColor(Color.parseColor("white"));
+            datanasabah.setTextColor(Color.parseColor("#f3801f"));
+            lldaftarnasabah.setBackgroundColor(Color.TRANSPARENT);
+            daftarnasabah.setTextColor(Color.parseColor("white"));
+        }
+    });
+    lldaftarnasabah.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            fm = getSupportFragmentManager().beginTransaction();
+            fm.replace(R.id.framenasabah,new DaftarNasabahFragment());
+            fm.commit();
+            lldaftarnasabah.setBackgroundColor(Color.parseColor("white"));
+            daftarnasabah.setTextColor(Color.parseColor("#f3801f"));
+            lldatanasabah.setBackgroundColor(Color.TRANSPARENT);
+            datanasabah.setTextColor(Color.parseColor("white"));
+        }
+    });
+    }
+
+    void findViewById() {
+        framenasabah=findViewById(R.id.framenasabah);
+        lldatanasabah=findViewById(R.id.lldatanasabah);
+        lldaftarnasabah=findViewById(R.id.lldaftarnasabah);
+        datanasabah=findViewById(R.id.datanasabah);
+        daftarnasabah=findViewById(R.id.daftarnasabah);
     }
 }
