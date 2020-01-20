e9b072835ba64182967bb197a2cd49d1dc6cbac7
haekalzain <haekalzain77@gmail.com>
2020-01-20 16:17:25 +0700

    push form peminjaman

---
+++ Final_Project/app/src/main/java/com/example/final_project/controller/PembayaranActivity.java
@@ -0,0 +1,14 @@
+package com.example.final_project.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PembayaranActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran);
+    }
+}
---
+++ Final_Project/app/src/main/java/com/example/final_project/rest/ApiInterface.java
@@ -0,0 +1,4 @@
+package com.example.final_project.rest;
+
+public interface ApiInterface {
+}
